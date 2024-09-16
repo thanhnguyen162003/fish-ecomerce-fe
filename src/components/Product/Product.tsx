daf7c1d1d1fc57d5a09918cc6291806772826795
thanhnguyen <thanhnxse1692003@gmail.com>
2024-09-16 08:03:24 +0700

    ok

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
-import Image from 'next/image'
+import Image from 'next/image' 
 import Link from 'next/link'
 import { ProductType } from '@/type/ProductType'
 import * as Icon from "@phosphor-icons/react/dist/ssr";
